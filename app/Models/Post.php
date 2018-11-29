b3d5af06f6e0dd3e34b5ce8a47060c59d155f17b
Jody Almaida Putra <jody.almaida@gmail.com>
2018-11-29 09:44:08 +0700

    initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+	protected $fillable = [
+        'title',
+        'body',
+        'slug',
+        'author_id',
+        'category_id',
+        // 'is_published'
+    ];
+}
