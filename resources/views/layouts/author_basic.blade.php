529e7fed2f95a39c1a5155cd73c934582c196b92
Jody Almaida Putra <jody.almaida@gmail.com>
2018-12-31 19:26:35 +0700

    new adsense

--- resources/views/layouts/author_basic.blade.php
+++ resources/views/layouts/author_basic.blade.php
@@ -2,18 +2,11 @@
 <html lang="en">
   <head>
   	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131112518-1"></script>
-	<script>
-  		window.dataLayer = window.dataLayer || [];
-  		function gtag(){dataLayer.push(arguments);}
-  		gtag('js', new Date());
-
-  		gtag('config', 'UA-131112518-1');
-	</script>
+	{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131112518-1"></script> --}}
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 		(adsbygoogle = window.adsbygoogle || []).push({
-			google_ad_client: "ca-pub-9901461487803247",
+			google_ad_client: "ca-pub-9327964152725536",
 			enable_page_level_ads: true
 		});
 	</script>
