a4a690357074053a5b99e40b77d30da412b8606a
Jody Almaida Putra <jody.almaida@gmail.com>
2019-01-01 02:00:53 +0700

    tambah meta google site

--- resources/views/layouts/author_basic.blade.php
+++ resources/views/layouts/author_basic.blade.php
@@ -13,6 +13,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="7gpPFkKxYhxoTmV9mEfRjQsNw4bp1I-y-uy976_fwdA" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="@yield('meta_description')">
     <meta name="keywords" content="@yield('meta_keywords')">
