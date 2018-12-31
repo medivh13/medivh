7a5984231d181c32431aa309491776a841d52f3c
Jody Almaida Putra <jody.almaida@gmail.com>
2018-12-31 20:13:01 +0700

    new adsense script yg bener

--- resources/views/layouts/author_basic.blade.php
+++ resources/views/layouts/author_basic.blade.php
@@ -6,7 +6,7 @@
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
 		(adsbygoogle = window.adsbygoogle || []).push({
-			google_ad_client: "ca-pub-9327964152725536",
+			google_ad_client: "ca-pub-9901461487803247",
 			enable_page_level_ads: true
 		});
 	</script>
