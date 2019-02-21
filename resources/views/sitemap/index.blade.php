d152d7bd941332ecc23a510604cde3528484c3cd
Almaida Jody <jody.almaida@gmail.com>
2019-02-21 14:39:32 +0700

    sitemap

---
+++ resources/views/sitemap/index.blade.php
@@ -0,0 +1,12 @@
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    @foreach($post as $key => $post)
+        <url>
+            <loc>{{ url("/post/{$post->slug}") }}</loc>
+            <lastmod>{{ $post->created_at->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>1.0</priority>
+        </url>
+    @endforeach
+</urlset>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use App\Models\Category;
 */
 // Route::resource('/', 'FrontEnd\BlogController@index');
 // Route::get('/post/{slug}', 'FrontEnd\BlogController@post');
+Route::get('/sitemap.xml', 'SiteMap\SiteMapController@index');
+
 Route::get('/', function (Request $request) {
 	$post= Post::when($request->search, function($query) use($request) {
 		$search = $request->search;
